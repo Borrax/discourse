f9f02b2d5c3a6d6e2d76eb52d5ed90fb7a1a5606
Borrax <borrax@protonmail.com>
2023-05-21 23:02:36 +0300

    Setting up html webpack plugin

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -1,13 +1,19 @@
 import type { Configuration } from 'webpack'
 import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 const config: Configuration = {
   mode: 'development',
   entry: './src/client.ts',
   output: {
     filename: 'client.bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, './dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Discourse'
+    })
+  ]
 }
 
 export default config
