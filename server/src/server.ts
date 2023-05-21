8d22dc638765822492fc5807275b7c588dd0caf8
Borrax <borrax@protonmail.com>
2023-05-21 21:57:26 +0300

    Adding a rudimentary express server to the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log('hello from the server side')
+import type { Response } from 'express'
+import express from 'express'
+
+const app = express()
+const PORT: number = 3000
+
+app.get('/', (_req, res: Response) => {
+  res.send('Hello from express')
+})
+
+app.listen(PORT, () => {
+  console.log('Server listening on port', PORT)
+})
