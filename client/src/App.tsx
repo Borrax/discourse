60e277b2b4c3b5c2eadfea731b76827608a8f50e
Borrax <borrax@protonmail.com>
2023-05-22 23:29:18 +0300

    Creating a rudimentary Home page component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Home } from './pages/Home'
+
 export const App = (): JSX.Element => {
   return (
   <>
     <h1>This is the app component</h1>
+    <Home />
   </>
   )
 }
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export const Home = (): JSX.Element => {
+  return (
+    <section>
+      <h2>This is the Home component</h2>
+    </section>
+  )
+}
