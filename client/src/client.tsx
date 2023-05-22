7c026bf4238b4f507ccb291469d62e57c34b8467
Borrax <borrax@protonmail.com>
2023-05-22 20:59:12 +0300

    Adding a rudimentary app component to the client side

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+import { App } from './components/App'
 
 const rootId: string = 'root'
 
@@ -8,7 +9,7 @@ const rootDomEl = document.getElementById(rootId)
 
 if (rootDomEl !== null) {
   const root = createRoot(rootDomEl)
-  root.render(<h1>Hello there</h1>)
+  root.render(<App />)
 } else {
   throw new Error('The doom root element is non-existent')
 }
---
+++ client/src/components/App.tsx
@@ -0,0 +1,7 @@
+export const App = (): JSX.Element => {
+  return (
+  <>
+    <h1>This is the app component</h1>
+  </>
+  )
+}
