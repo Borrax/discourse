d3897443f0743c381efb1dba07a6a65cf3afcd44
Borrax <borrax@protonmail.com>
2023-05-22 23:06:27 +0300

    Setting up sass to the client side

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+import './styles/index.scss'
+
 const rootId: string = 'root'
 
 document.body.innerHTML = `<div id="${rootId}"></div>`
