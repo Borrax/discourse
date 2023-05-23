f7fb711b02c49974a7cc5cf5f2dbc133db126059
Borrax <borrax@protonmail.com>
2023-05-23 22:05:43 +0300

    Serving static files from the client

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,7 +1,11 @@
-import type { Application, Response } from "express";
+import type { Application, Response } from "express"
+import express from 'express'
+import path from 'path'
 
 export const attachRoutes = (app: Application) => {   
+  app.use(express.static(path.resolve(__dirname, '../../../client/dist/')))
+
   app.get('/', (_req, res: Response) => {
-    res.send('Hello from express')
+    res.sendFile(path.resolve(__dirname, '../../../client/dist/index.html'))
   })
 } 
