3ab1e8189879c00c90a9e572ba284ac56bdcb6b3
Borrax <borrax@protonmail.com>
2023-05-24 19:11:26 +0300

    Refactofing a bit the method initializing the server routes

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,11 +1,17 @@
-import type { Application, Response } from "express"
+import type { Application, Response } from 'express'
 import express from 'express'
 import path from 'path'
 
-export const attachRoutes = (app: Application) => {   
-  app.use(express.static(path.resolve(__dirname, '../../../client/dist/')))
+const staticDirDest = path.resolve(__dirname, '../../../client/dist/')
+
+/**
+* @function A function responsible to attaching all the server routes to the express app
+* @params app: The express app instance
+*/
+export const attachRoutes = (app: Application): void => {
+  app.use(express.static(staticDirDest))
 
   app.get('/', (_req, res: Response) => {
-    res.sendFile(path.resolve(__dirname, '../../../client/dist/index.html'))
+    res.sendFile(path.join(staticDirDest, './index.html'))
   })
-} 
+}
