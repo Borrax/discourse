4a6eff4ca038350b588a06ca75f70468be888159
Borrax <borrax@protonmail.com>
2023-05-24 22:57:48 +0300

    Handling some db connection events

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,11 +1,19 @@
 import mongoose from 'mongoose'
 
 const connectToDb = async () => {
-  const connectionUrl = 'mongodb://127.0.0.1:27017/discourse'
-  return await mongoose.connect(connectionUrl)
+  const dbUrl = 'mongodb://127.0.0.1:27017/discourse'
+  return await mongoose.connect(dbUrl)
 }
 
 export const initializeDb = () => {
-  connectToDb().then(() => console.log('connected to the db'))
+  mongoose.connection.on('connecting', () => {
+    console.log('Connecting to the DB')
+  })
+
+  mongoose.connection.on('error', err => {
+    console.error(err)
+  })
+
+  connectToDb().then(() => console.log('Connected to the DB'))
   .catch(err => console.error(err))
 }
