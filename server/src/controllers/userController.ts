fe92c50222056e9e1d1d7b68253b122390ff2604
Borrax <borrax@protonmail.com>
2023-05-24 19:26:09 +0300

    Adding a rudimentary controller for the user registration

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from 'express';
+
+const register = (_req: Request, res: Response): void => {
+  res.send('Not implemented yet')
+}
+
+export const UserController = {
+  register
+}
