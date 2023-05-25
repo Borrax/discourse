e73f0cace22ba9ce2af22044cae936dd0c3d5ed8
Borrax <borrax@protonmail.com>
2023-05-25 21:17:25 +0300

    Validating the user registration entry data

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,33 @@
 import type { Request, Response } from 'express'
+import type { UserRegData } from 'shared/UserSharedTypes'
 
-const register = (_req: Request, res: Response): void => {
-  res.send('Not implemented yet')
+const isValidRegData = (regData: UserRegData | null): boolean => {
+  if (regData === null || typeof regData !== 'object') {
+    return false
+  }
+
+  const { username, password } = regData
+
+  if (username == null || typeof username !== 'string') {
+    return false
+  }
+
+  if (password == null || typeof password !== 'string') {
+    return false
+  }
+
+  return true
+}
+
+const register = (req: Request, res: Response): void => {
+  const regData: UserRegData | null = req.body
+
+  if (!isValidRegData(regData)) {
+    res.send({ err: 'Invalid registration data' })
+    return
+  }
+
+  res.send(req.body)
 }
 
 export const UserController = {
