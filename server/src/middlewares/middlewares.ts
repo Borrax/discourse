b80ed24b114a02810f577b3854c3c447a801a345
Borrax <borrax@protonmail.com>
2023-05-25 21:58:57 +0300

    Adding an express middleware for handling of error

---
+++ server/src/middlewares/middlewares.ts
@@ -0,0 +1,27 @@
+import type { Application, NextFunction,
+  Request, Response } from "express";
+
+const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err)
+
+  if (err.status === 400) {
+    res.status(400).json({ error: 'Invalid data format sent to the server' })
+    return
+  }
+   
+  if (err) {
+    res.status(500).json({ error: 'Internal server error'})
+    return
+  }
+
+  next()
+}
+
+export const initializeMiddlewares = (app: Application) => {
+  app.use(errorHandler)
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import { attachRoutes } from './routes/routes'
 import { initializeDb } from './db/db'
+import { initializeMiddlewares } from './middlewares/middlewares'
 
 const app = express()
 const PORT: number = 8000
 
 app.use(express.json())
-attachRoutes(app)
 
+initializeMiddlewares(app)
+attachRoutes(app)
 initializeDb()
 
 app.listen(PORT, () => {
