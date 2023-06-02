dd8cb828f9dd53f56ced23d0fd1fbf5f820b7cc9
Borrax <borrax@protonmail.com>
2023-06-02 19:31:44 +0300

    Defining the type of the user registration success response payload

--- shared/UserSharedTypes.ts
+++ shared/UserSharedTypes.ts
@@ -1,7 +1,7 @@
 import type { ServerResponse } from './ServerResponseTypes'
 
 /**
- * @type The type of data that is needed by
+* @type The type of data that is needed by
 * the server to register the user
 */
 export type UserRegData = {
@@ -9,6 +9,16 @@ export type UserRegData = {
   password: string
 }
 
-export type UserRegResponse = ServerResponse<{
-  username: String
-}>
+/**
+* @type The type of the payload that the server will send to the 
+* client after a successful registration
+*/
+export type UserRegResponseLoad = {
+  username: string
+}
+
+/**
+* @type The type of the server response the client will get
+* upon user registration call
+*/
+export type UserRegResponse = ServerResponse<UserRegResponseLoad>
