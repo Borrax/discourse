bdec04adf7895b45a5b826c427a8c40b96255e01
Borrax <borrax@protonmail.com>
2023-06-05 22:45:35 +0300

    Limiting the number of the jest workers to avoid server init clashes

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -6,6 +6,7 @@
 export default {
   rootDir: './dist/server/src/tests/',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  maxWorkers: 1,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
