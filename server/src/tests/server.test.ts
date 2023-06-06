557f8c0c71d9b7ce978d8167944e5e092f24d69c
Borrax <borrax@protonmail.com>
2023-06-06 18:00:29 +0300

    Removing the top level describe function of the server test

--- server/src/tests/server.test.ts
+++ server/src/tests/server.test.ts
@@ -4,69 +4,67 @@ import supertest from 'supertest'
 
 const request = supertest.agent(app)
 
-describe('Express server testing', () => {
-  describe('Testing GET to /', () => {
-    it('should return status 200', async () => {
-      const resp = await request.get('/')
-      expect(resp.status).toBe(200)
-    })
-
-    it('should return an html file', async () => {
-      const resp = await request.get('/')
-      const contentType = resp.headers['content-type']
-      expect(contentType).toContain('text/html')
-    })
+describe('Testing GET to /', () => {
+  it('should return status 200', async () => {
+    const resp = await request.get('/')
+    expect(resp.status).toBe(200)
   })
 
-  describe('Testing server\'s ability to handle json', () => {
-    it('should return status 400 when bad json is sent', async () => {
-      const badJsonStr = `{
-        "test": testing
-        "subject": "bad json"
-      }`
+  it('should return an html file', async () => {
+    const resp = await request.get('/')
+    const contentType = resp.headers['content-type']
+    expect(contentType).toContain('text/html')
+  })
+})
 
-      jest.spyOn(console, 'error').mockImplementation(() => {})
+describe('Testing server\'s ability to handle json', () => {
+  it('should return status 400 when bad json is sent', async () => {
+    const badJsonStr = `{
+      "test": testing
+      "subject": "bad json"
+    }`
 
-      const resp = await request.post('/test/json')
-        .send(badJsonStr)
-        .set('content-type', 'application/json')
+    jest.spyOn(console, 'error').mockImplementation(() => {})
 
-      expect(resp.status).toBe(400)
-    })
+    const resp = await request.post('/test/json')
+      .send(badJsonStr)
+      .set('content-type', 'application/json')
 
-    it('should display an error in the console when an invalid json is provided', async () => {
-      const badJsonStr = `{
-        "test": testing
-        "subject": "bad json"
-      }`
+    expect(resp.status).toBe(400)
+  })
 
-      // surpressing the displaying of the console error for
-      // his test
-      jest.spyOn(console, 'error').mockImplementation(() => {})
+  it('should display an error in the console when an invalid json is provided', async () => {
+    const badJsonStr = `{
+      "test": testing
+      "subject": "bad json"
+    }`
 
-      await request.post('/test/json')
-        .send(badJsonStr)
-        .set('content-type', 'application/json')
+    // surpressing the displaying of the console error for
+    // his test
+    jest.spyOn(console, 'error').mockImplementation(() => {})
 
-      expect(console.error).toHaveBeenCalled()
-    })
+    await request.post('/test/json')
+      .send(badJsonStr)
+      .set('content-type', 'application/json')
 
-    it('should return the same json that it has received', async () => {
-      const testObj = {
-        test: 'test',
-        arr: ['item1', 2],
-        empty: '',
-        num: 4
-      }
+    expect(console.error).toHaveBeenCalled()
+  })
 
-      const resp = await request.post('/test/json')
-        .send(testObj)
-        .set('content-type', 'application/json')
-        .set('Accept', 'application/json')
+  it('should return the same json that it has received', async () => {
+    const testObj = {
+      test: 'test',
+      arr: ['item1', 2],
+      empty: '',
+      num: 4
+    }
 
-      const contentType = resp.headers['content-type']
-      expect(contentType).toContain('application/json')
-      expect(resp.body).toEqual(testObj)
-    })
+    const resp = await request.post('/test/json')
+      .send(testObj)
+      .set('content-type', 'application/json')
+      .set('Accept', 'application/json')
+
+    const contentType = resp.headers['content-type']
+    expect(contentType).toContain('application/json')
+    expect(resp.body).toEqual(testObj)
   })
 })
