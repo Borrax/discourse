a03d950f58e027af61eb61385272177e71e4428b
Borrax <borrax@protonmail.com>
2023-06-06 17:44:26 +0300

    Defining a shared function that determines if an object is a server success response obj

--- shared/serverResponseMethods.ts
+++ shared/serverResponseMethods.ts
@@ -1,5 +1,12 @@
 import type { ErrorResponse, SuccessResponse } from './ServerResponseTypes'
 
+export const isSuccessResponseObj = <T>(obj: SuccessResponse<T> | null): boolean => {
+  if (obj === null) return false
+  if ('load' in obj) return true
+
+  return false
+}
+
 export const isErrorResponseObj = (resp: object | null): boolean => {
   if (resp === null) return false
   if ('err' in (resp as ErrorResponse)) return true
