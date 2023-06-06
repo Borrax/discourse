b571fd6d731a0ce7f2419da80e66bcd700c72f55
Borrax <borrax@protonmail.com>
2023-06-06 18:38:13 +0300

    More extensive server tests when the registering user already exists

--- server/src/tests/user/register.test.ts
+++ server/src/tests/user/register.test.ts
@@ -14,10 +14,32 @@ const existingUser: UserRegData = {
 const request = supertest.agent(globalThis.__SERVER__)
 
 describe('Testing the user registration API at ' + registerPath, () => {
-  it('should return an error response when the username exists', async () => {
+  it('should return a status 400 when the user already exists', async () => {
+    const resp = await request.post(registerPath)
+      .send(existingUser)
+    expect(resp.status).toBe(400)
+  })
+
+  it('should return an error response object when the username exists', async () => {
+    const resp = await request.post(registerPath)
+      .send(existingUser)
+    expect(isErrorResponseObj(resp.body)).toBe(true)
+  })
+
+  it('should contain an error message string when the user already exists', async () => {
     const resp = await request.post(registerPath)
       .send(existingUser)
 
-    expect(isErrorResponseObj(resp.body)).toBe(true)
+    expect(resp.body.err).toBeDefined()
+    expect(typeof resp.body.err).toBe('string')
+    expect(resp.body.err.length).toBeGreaterThan(0)
+  })
+  it(`should include words 'registered' or 'exists' in 
+error message string when the user already exists`, async () => {
+    const resp = await request.post(registerPath)
+      .send(existingUser)
+    const regex = /(exists)|(registered)/g
+
+    expect(regex.test(resp.body.err)).toBe(true)
   })
 })
