e8fd6a5efab02119b91adeb3f8186bd9620ac292
Borrax <borrax@protonmail.com>
2023-06-08 12:34:08 +0300

    Changing the block and test names of the user already exists registration tests

--- server/src/tests/user/register.test.ts
+++ server/src/tests/user/register.test.ts
@@ -65,20 +65,20 @@ describe('Testing the user registration API at ' + registerPath, () => {
     })
   })
 
-  it('should return a status 400 when the user already exists', async () => {
-    const resp = await request.post(registerPath)
-      .send(existingUser)
-    expect(resp.status).toBe(400)
-  })
-  
-  describe('Testing when user is invalid', () => {
-    it('should return an error response object when the username exists', async () => {
+  describe('Testing when user already exists', () => {
+    it('should return a status 400', async () => {
+      const resp = await request.post(registerPath)
+        .send(existingUser)
+      expect(resp.status).toBe(400)
+    })
+
+    it('should return an error response object', async () => {
       const resp = await request.post(registerPath)
         .send(existingUser)
       expect(isErrorResponseObj(resp.body)).toBe(true)
     })
 
-    it('should contain an error message string when the user already exists', async () => {
+    it('should contain an error message string', async () => {
       const resp = await request.post(registerPath)
         .send(existingUser)
 
@@ -87,8 +87,7 @@ describe('Testing the user registration API at ' + registerPath, () => {
       expect(resp.body.err.length).toBeGreaterThan(0)
     })
 
-    it(`should include words 'registered' or 'exists' in 
-  error message string when the user already exists`, async () => {
+    it('should include words \'registered\' or \'exists\' in the error message string', async () => {
       const resp = await request.post(registerPath)
         .send(existingUser)
       const regex = /(exists)|(registered)/g
