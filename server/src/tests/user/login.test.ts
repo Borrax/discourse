de5e6d185a773812ac9a315cab97bcd723b283ec
Borrax <borrax@protonmail.com>
2023-06-10 23:48:43 +0300

    Adding more tests for when a non-existent user tries to login

--- server/src/tests/user/login.test.ts
+++ server/src/tests/user/login.test.ts
@@ -3,21 +3,38 @@ import supertest from 'supertest'
 import { describe, it, expect } from '@jest/globals'
 import { apiPaths } from '../../../../shared/apiPaths'
 import { app } from '../../server'
+import { isErrorResponseObj } from '../../serverResponseMethods'
 
 const request = supertest(app)
 
-describe('Testing the user login at ' + apiPaths.user.login , () => {
-  describe('Testing invalid requests', () => {
-    it('should return status 400 when the user doesn\'t exist', async () => {
-      const nonExistingUser: UserLoginData = {
-        username: 'someUser',
-        password: 'somePass'
-      }
+const nonExistingUser: UserLoginData = {
+  username: 'someUser',
+  password: 'somePass'
+}
 
+describe('Testing the user login at ' + apiPaths.user.login , () => {
+  describe('Testing when user doesn\'t exist', () => {
+    it('should return status 400 when the user doesn\'t exist', async () => {
       const resp = await request.post(apiPaths.user.login)
         .send(nonExistingUser)
 
       expect(resp.status).toBe(400)
     })
+  })
+
+  it('should return an error response object', async () => {
+      const resp = await request.post(apiPaths.user.login)
+        .send(nonExistingUser)
+
+      expect(isErrorResponseObj(resp.body)).toBe(true)
+  })
+
+  it('should contain the words \'exists\' or \'registered\' in the error'
+  + 'message', async () => {
+      const regex = /(exists)|(registered)/ig
+      const resp = await request.post(apiPaths.user.login)
+        .send(nonExistingUser)
+      
+      expect(regex.test(resp.body.err)).toBe(true)
   })
 })
