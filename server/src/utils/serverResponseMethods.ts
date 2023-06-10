7cd7ecdc8686a0c3b990ec7eea55c8f6e3b33173
Borrax <borrax@protonmail.com>
2023-06-10 17:38:06 +0300

    Server response object creators jsdoc comments

--- server/src/utils/serverResponseMethods.ts
+++ server/src/utils/serverResponseMethods.ts
@@ -1,11 +1,21 @@
 import type { ErrorResponse, SuccessResponse } from '../../../shared/types/ServerResponseTypes'
 
+/**
+* @function A function that creates an object of an error server response type
+* that will be sent to the client
+* @returns An error server response onject
+*/
 export const createErrorResponseObj = (msg: string): ErrorResponse => {
   return {
     err: msg
   }
 }
 
+/**
+* @function A function that creates an object of an success server response type
+* that will be sent to the client
+* @returns An success server response onject
+*/
 export const createSuccessResponseObj = <T>(load: T): SuccessResponse<T> => {
   return {
     load
