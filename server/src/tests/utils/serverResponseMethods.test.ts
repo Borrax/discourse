fc4bb16304faf1d91e684f570713223cafd01042
Borrax <borrax@protonmail.com>
2023-06-13 13:27:38 +0300

    Adding tests for the success server response object creator

--- server/src/tests/utils/serverResponseMethods.test.ts
+++ server/src/tests/utils/serverResponseMethods.test.ts
@@ -1,12 +1,12 @@
-import { describe, test, expect } from '@jest/globals'
-import { createErrorResponseObj } from '../../utils/serverResponseMethods'
-import { isErrorResponseObj } from '../../../../shared/serverResponseMethods'
+import { describe, test, expect, it } from '@jest/globals'
+import { createErrorResponseObj, createSuccessResponseObj } from '../../utils/serverResponseMethods'
+import { isErrorResponseObj, isSuccessResponseObj } from '../../../../shared/serverResponseMethods'
 
 describe('Testing the server response methods', () => {
   describe('The error response obj creator', () => {
     const errorMsg = 'test error'
 
-    test('to throw an error when input is', () => {
+    test('to throw an error when input is undefined', () => {
       expect(() => createErrorResponseObj(undefined as any))
         .toThrow(TypeError)
     })
@@ -37,6 +37,39 @@ describe('Testing the server response methods', () => {
     test('to return error response obj when a non-empty string is passed', () => {
       const respObj = createErrorResponseObj(errorMsg)
       expect(isErrorResponseObj(respObj)).toBe(true)
+    })
+  })
+
+  describe('Testing the success response obj creator', () => {
+    it('should return a success object when null is provided', () => {
+      const resp = createSuccessResponseObj(null)
+      expect(isSuccessResponseObj(resp)).toBe(true)
+      expect(resp.load).toBe(null)
+    })
+
+    it('should return a success object when a number is provided', () => {
+      const resp = createSuccessResponseObj(50001)
+      expect(isSuccessResponseObj(resp)).toBe(true)
+      expect(typeof resp.load).toBe('number')
+    })
+
+    it('should return a success object when a string is provided', () => {
+      const resp = createSuccessResponseObj('test string')
+      expect(isSuccessResponseObj(resp)).toBe(true)
+      expect(typeof resp.load).toBe('string')
+    })
+
+    it('should return a success object when an object is provided', () => {
+      const resp = createSuccessResponseObj({ test: true })
+      expect(isSuccessResponseObj(resp)).toBe(true)
+      expect(typeof resp.load).toBe('object')
+      expect(resp.load?.test).toBe(true)
+    })
+
+    it('should return a success object when an array is provided', () => {
+      const resp = createSuccessResponseObj([1, 2, '3'])
+      expect(isSuccessResponseObj(resp)).toBe(true)
+      expect(Array.isArray(resp.load)).toBe(true)
     })
   })
 })
