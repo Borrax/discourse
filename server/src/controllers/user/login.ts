7bcf65fc289dadbc510aeeaf9172d7348fc6ed8f
Borrax <borrax@protonmail.com>
2023-06-14 14:40:33 +0300

    Validating the user login fields on the server side

--- server/src/controllers/user/login.ts
+++ server/src/controllers/user/login.ts
@@ -1,10 +1,18 @@
 import type { RequestHandler } from 'express'
 import type { ServerResponse } from '../../../../shared/types/ServerResponseTypes'
 import type { UserLoginLoad } from '../../types/UserSharedTypes'
-import { createSuccessResponseObj } from '../../utils/serverResponseMethods'
+import { createErrorResponseObj, createSuccessResponseObj } from '../../utils/serverResponseMethods'
+import { findErrInLoginData } from '../../../../shared/loginDataValidator'
 
-export const login = (((_req, res) => {
+export const login = (((req, res) => {
   let serverResponse: ServerResponse<UserLoginLoad> | null = null
+
+  const errorMsg = findErrInLoginData(req.body)
+  if (errorMsg !== null) {
+    serverResponse = createErrorResponseObj(errorMsg)
+    res.status(400).json(serverResponse)
+    return
+  }
 
   serverResponse = createSuccessResponseObj({
     token: ''
