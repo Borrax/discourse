965f8a2b487fe585579241a6e9bea6317bff0293
Borrax <borrax@protonmail.com>
2023-06-14 14:33:58 +0300

    Returning a success response on login request

--- server/src/controllers/user/login.ts
+++ server/src/controllers/user/login.ts
@@ -1,5 +1,13 @@
 import type { RequestHandler } from 'express'
+import type { ServerResponse } from '../../../../shared/types/ServerResponseTypes'
+import type { UserLoginLoad } from '../../types/UserSharedTypes'
+import { createSuccessResponseObj } from '../../utils/serverResponseMethods'
 
 export const login = (((_req, res) => {
-  res.json({ msg: 'this is the logn route' })
+  let serverResponse: ServerResponse<UserLoginLoad> | null = null
+
+  serverResponse = createSuccessResponseObj({
+    token: ''
+  })
+  res.json(serverResponse)
 }) as RequestHandler)
