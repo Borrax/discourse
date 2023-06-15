6c304d6d5a13cdb2dcee5329880893a49003eab6
Borrax <borrax@protonmail.com>
2023-06-15 23:41:49 +0300

    Adding a method that generates a hashed password with salt

--- server/src/utils/hash.ts
+++ server/src/utils/hash.ts
@@ -1,5 +1,30 @@
-import { createHash } from 'crypto'
+import { createHash, scrypt, randomBytes } from 'crypto'
 
 export const genPassHash = (plainPass: string): string => {
   return createHash('sha256').update(plainPass).digest('hex')
 }
+
+export const genPassHashWSalt = async (pass: string): Promise<{
+  hashedPassWSalt: string,
+  salt: string
+}> => {
+  const salt = randomBytes(16).toString('hex')
+  const hashedPassWSaltBuff = await new Promise<Buffer>((resolve, reject) => {
+    scrypt(pass, salt, 64, (err, derivedKey) => {
+      if (err !== null) {
+        reject(err)
+        return
+      }
+
+      resolve(derivedKey)
+    })
+  })
+
+  const hashedPassWSalt = hashedPassWSaltBuff.toString('hex')
+  
+  return {
+    hashedPassWSalt,
+    salt
+  }
+}
+
