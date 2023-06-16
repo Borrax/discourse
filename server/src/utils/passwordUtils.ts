b1a8f2673c3d9555c4a1fc185ec375d2d6b9ac8b
Borrax <borrax@protonmail.com>
2023-06-16 23:44:50 +0300

    Defining a method that would create the final hashed password for the db user

--- server/src/utils/passwordUtils.ts
+++ server/src/utils/passwordUtils.ts
@@ -1,13 +1,13 @@
 import { scrypt, randomBytes } from 'crypto'
 import { errorLogger } from './errorLogger'
 
-export const genPassHashWSalt = async (pass: string): Promise<{
-  hashedPassWSalt: string
+const genPassHashNSalt = async (plainPass: string): Promise<{
+  hashedPass: string
   salt: string
 } | null> => {
   const salt = randomBytes(16).toString('hex')
-  const hashedPassWSaltBuff = await new Promise<Buffer>((resolve, reject) => {
-    scrypt(pass, salt, 64, (err, derivedKey) => {
+  const hashedPassBuff = await new Promise<Buffer>((resolve, reject) => {
+    scrypt(plainPass, salt, 64, (err, derivedKey) => {
       if (err !== null) {
         reject(err)
         return
@@ -19,14 +19,26 @@ export const genPassHashWSalt = async (pass: string): Promise<{
     errorLogger(new Error('Error while creating the password hash with salt', err))
   })
 
-  if (hashedPassWSaltBuff === undefined) {
+  if (hashedPassBuff === undefined) {
     return null
   }
 
-  const hashedPassWSalt = hashedPassWSaltBuff.toString('hex')
+  const hashedPass = hashedPassBuff.toString('hex')
 
   return {
-    hashedPassWSalt,
+    hashedPass,
     salt
   }
+}
+
+export const genHashedPassNSaltStr = () => async (plainPass: string): Promise<
+string | null> => {
+  const hashedPassNSaltObj = await genPassHashNSalt(plainPass)
+
+  if (hashedPassNSaltObj === null) {
+    return null
+  }
+
+  return hashedPassNSaltObj.hashedPass +
+    ':' + hashedPassNSaltObj.salt
 }
