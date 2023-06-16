9b24fbce921ff43e5b15efaff43156537c673880
Borrax <borrax@protonmail.com>
2023-06-16 23:56:14 +0300

    Refactoring of the password hash and salt creation

--- server/src/utils/passwordUtils.ts
+++ server/src/utils/passwordUtils.ts
@@ -1,11 +1,7 @@
 import { scrypt, randomBytes } from 'crypto'
 import { errorLogger } from './errorLogger'
 
-const genPassHashNSalt = async (plainPass: string): Promise<{
-  hashedPass: string
-  salt: string
-} | null> => {
-  const salt = randomBytes(16).toString('hex')
+const genHashedPass = async (plainPass: string, salt: string): Promise<string | null> => {
   const hashedPassBuff = await new Promise<Buffer>((resolve, reject) => {
     scrypt(plainPass, salt, 64, (err, derivedKey) => {
       if (err !== null) {
@@ -23,22 +19,16 @@ const genPassHashNSalt = async (plainPass: string): Promise<{
     return null
   }
 
-  const hashedPass = hashedPassBuff.toString('hex')
-
-  return {
-    hashedPass,
-    salt
-  }
+  return hashedPassBuff.toString('hex')
 }
 
-export const genHashedPassNSaltStr = () => async (plainPass: string): Promise<
-string | null> => {
-  const hashedPassNSaltObj = await genPassHashNSalt(plainPass)
+export const genHashedPassNSaltStr = async (plainPass: string): Promise<string | null> => {
+  const salt = randomBytes(16).toString('hex')
+  const hashedPass = await genHashedPass(plainPass, salt)
 
-  if (hashedPassNSaltObj === null) {
+  if (hashedPass === null) {
     return null
   }
 
-  return hashedPassNSaltObj.hashedPass +
-    ':' + hashedPassNSaltObj.salt
+  return hashedPass + ':' + salt
 }
