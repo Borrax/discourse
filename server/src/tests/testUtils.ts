e47fab3903666423fe7512d5092e997745f74f86
Borrax <borrax@protonmail.com>
2023-06-16 17:36:37 +0300

    Polishing the jsdoc and changing the regex of the random string generator

--- server/src/tests/testUtils.ts
+++ server/src/tests/testUtils.ts
@@ -1,12 +1,13 @@
 /**
 * @function Generates a random string of given length.
-* Note that the characters used are from 33 to 127 of
+* Note that the characters used are from 32 to 127 of
 * the ASCII table
 * @param len - The length of the desired string
 * @param testRegex - The regex of the allowed characters
-* in the random string
+* in the random string (optional). By default it will
+* include any character between 32 and 127
 */
-export const genRandomString = (len: number, testRegex: RegExp = /[w]+/): string => {
+export const genRandomString = (len: number, testRegex: RegExp = /./): string => {
   if (!Number.isInteger(len)) {
     throw new TypeError('Expected a number for the len argument but got ' +
       typeof len)
@@ -18,14 +19,13 @@ export const genRandomString = (len: number, testRegex: RegExp = /[w]+/): string
   }
   
   let allowedChars = ''
-  for (let i = 33; i < 127; i++) {
+  for (let i = 32; i < 127; i++) {
     const c = String.fromCharCode(i)
     if (testRegex.test(c)) {
       allowedChars += c
     }
   }
 
-  console.log('allowed chars', allowedChars)
   let resultStr: string = ''
   for(let i = 0; i < len; i++) {
     const randomIndex = Math.round(Math.random() * allowedChars.length)
@@ -34,3 +34,5 @@ export const genRandomString = (len: number, testRegex: RegExp = /[w]+/): string
 
   return resultStr
 }
+
+console.log(genRandomString(10))
