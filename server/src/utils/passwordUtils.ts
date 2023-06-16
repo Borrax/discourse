6fb0e1cd427180f3b06dcb5901afa14bb8cd01eb
Borrax <borrax@protonmail.com>
2023-06-16 12:20:24 +0300

    Removing the sha256 based password hash generator

--- server/src/utils/passwordUtils.ts
+++ server/src/utils/passwordUtils.ts
@@ -1,12 +1,8 @@
-import { createHash, scrypt, randomBytes } from 'crypto'
+import { scrypt, randomBytes } from 'crypto'
 import { errorLogger } from './errorLogger'
 
-export const genPassHash = (plainPass: string): string => {
-  return createHash('sha256').update(plainPass).digest('hex')
-}
-
 export const genPassHashWSalt = async (pass: string): Promise<{
-  hashedPassWSalt: string,
+  hashedPassWSalt: string
   salt: string
 } | null> => {
   const salt = randomBytes(16).toString('hex')
@@ -20,15 +16,15 @@ export const genPassHashWSalt = async (pass: string): Promise<{
       resolve(derivedKey)
     })
   }).catch(err => {
-      errorLogger(new Error('Error while creating the password hash with salt', err))
-    })
+    errorLogger(new Error('Error while creating the password hash with salt', err))
+  })
 
   if (hashedPassWSaltBuff === undefined) {
     return null
   }
 
   const hashedPassWSalt = hashedPassWSaltBuff.toString('hex')
-  
+
   return {
     hashedPassWSalt,
     salt
