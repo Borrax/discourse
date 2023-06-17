f71b0e15eb290e54414164c9daf2931f57cf959d
Borrax <borrax@protonmail.com>
2023-06-17 22:38:00 +0300

    Adding methods that return exsisting/non-existing user data needed for the registration tests

--- server/src/tests/testUtils/usersUtils.ts
+++ server/src/tests/testUtils/usersUtils.ts
@@ -1,4 +1,4 @@
-import type { UserLoginData } from '../../../../shared/types/UserSharedTypes'
+import type { UserLoginData, UserRegData } from '../../../../shared/types/UserSharedTypes'
 
 /**
 * @function A function that yields the login data of an exsiting
@@ -12,12 +12,35 @@ export const getExistingUserLoginData = (): UserLoginData => {
 }
 
 /**
-* @function A function that returns login data of a user that doesn't
+* @function A function that returns valid login data of a user that doesn't 
 * in the test db
 */
 export const getNonExistentUserLoginData = (): UserLoginData => {
   return {
     username: 'someUser',
     password: 'somePass'
+  }
+}
+
+/**
+* @function A function that yields the data that is needed
+* for registration of an already existing in the test db
+* user
+*/
+export const getExistingUserRegData = (): UserRegData => {
+  return {
+    username: 'existing_username',
+    password: 'some_password'
+  }
+}
+
+/**
+* @function A function that yields the valid registration
+* data needed of an non-existing in the test DB user
+*/
+export const getNonExistingUserRegData = (): UserRegData => {
+  return {
+    username: 'testUsername',
+    password: 'testPassword'
   }
 }
