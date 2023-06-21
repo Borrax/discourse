3c05b88f1a8c74ccd51678e8ad6c08ca3b3583a0
Borrax <borrax@protonmail.com>
2023-06-21 23:26:30 +0300

    Removing the user register fields validator from shared dir

--- server/src/controllers/user/register.ts
+++ server/src/controllers/user/register.ts
@@ -1,10 +1,60 @@
 
 import type { RequestHandler } from 'express'
 import type { UserRegData, UserRegResponse, UserRegResponseLoad } from '../../../../shared/types/UserSharedTypes'
+
 import { User } from '../../models/user'
 import { isErrorResponseObj } from '../../../../shared/serverResponseMethods'
-import { findErrInRegData } from '../../../../shared/userRegDataValidator'
 import { createErrorResponseObj, createSuccessResponseObj } from '../../utils/serverResponseMethods'
+import { regDataValidationRegex, allowedUserRegLengths } from '../../../../shared/userConstraintsShared'
+
+const {
+  MIN_USERNAME_LEN, MAX_USERNAME_LEN,
+  MIN_PASSWORD_LEN, MAX_PASSWORD_LEN
+} = allowedUserRegLengths
+
+const { USERNAME_REGEX, PASSWORD_REGEX } = regDataValidationRegex
+
+/**
+* @function Goes through the the fields of the user registration
+* and looks for inconsistencies
+* @return A string with the error message if it finds
+* anything or null if it doesn't
+*/
+const findErrInRegData = (regData: UserRegData | null): string | null => {
+  if (regData === null || typeof regData !== 'object') {
+    return 'No registration data provided'
+  }
+
+  const { username, password } = regData
+
+  if (username == null || typeof username !== 'string') {
+    return 'Missing username'
+  }
+
+  if (username.length < MIN_USERNAME_LEN ||
+    username.length > MAX_USERNAME_LEN) {
+    return `Username should be between ${MIN_USERNAME_LEN} and ${MAX_USERNAME_LEN} characters long`
+  }
+
+  if (!USERNAME_REGEX.test(username)) {
+    return 'The username contains forbidden symbols'
+  }
+
+  if (password == null || typeof password !== 'string') {
+    return 'Missing password'
+  }
+
+  if (password.length < MIN_PASSWORD_LEN ||
+  password.length > MAX_PASSWORD_LEN) {
+    return `Password should be between ${MIN_PASSWORD_LEN} and ${MAX_PASSWORD_LEN} characters long`
+  }
+
+  if (!PASSWORD_REGEX.test(password)) {
+    return 'The password contains forbidden symbols'
+  }
+
+  return null
+}
 
 export const register = ((async (req, res) => {
   const regData: UserRegData | null = req.body
