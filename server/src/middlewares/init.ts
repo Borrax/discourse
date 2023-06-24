882990df06b46bc97aadced71fe0314ad228b230
Borrax <borrax@protonmail.com>
2023-06-24 21:31:41 +0300

    JSdoc comment for the function that attaches middlewares for every server http request

--- server/src/middlewares/init.ts
+++ server/src/middlewares/init.ts
@@ -3,6 +3,10 @@ import type { Application } from 'express';
 import { json } from 'express'
 import { errorHandlerMiddle } from './errorHandlerMiddle'
 
+/**
+* @function Makes the express app use different middlewares that
+* need to be present at any server request
+*/ 
 export const initGeneralMiddlewares = (app: Application): void => {
   const jsonMiddle = json()
 
