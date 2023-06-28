ebe3b826736e23a0d09a01c767e4f2bf58f6e1b5
Borrax <borrax@protonmail.com>
2023-06-28 21:11:47 +0300

    Adding a cookie parser test for multiple cookies

--- server/src/tests/middlewares/cookieParserMiddle.test.ts
+++ server/src/tests/middlewares/cookieParserMiddle.test.ts
@@ -1,6 +1,8 @@
 import supertest from 'supertest'
 import { describe, it, expect } from '@jest/globals'
 import { app } from '../../server'
+import { genRandomString } from '../testUtils/randomStrings'
+import { genRandomInt } from '../testUtils/randomNumber'
 
 type Cookie = {
   name: string
@@ -9,6 +11,10 @@ type Cookie = {
 
 describe('Testing the cookie parser middleware', () => {
   const testRoute = '/test/cookieParser'
+  const cookieAllowedChars = {
+    name: /^[^\x00-\0x1F\s?={}()\[\]<>,:;\\\/"@\x7F]+$/,
+    value: /^[^\x00-\0x1F\s=,:;\\\/"\x7F]+$/
+  }
 
   const request = async (cookies: Cookie[]) => {
     let cookieHeaderVal = ''
@@ -37,5 +43,31 @@ describe('Testing the cookie parser middleware', () => {
     expect(respCookies).toBeDefined()
     expect(respCookies[cookie.name]).toBeDefined()
     expect(respCookies[cookie.name]).toBe(cookie.value)
+  })
+
+  it('should process multiple cookies', async () => {
+    const cookies: Cookie[] = []
+
+    for (let i = 0; i < 10; i++) {
+      const nameLen = genRandomInt(1, 16)
+      const valueLen = genRandomInt(1, 32)
+      const c: Cookie = {
+        name: genRandomString(nameLen, cookieAllowedChars.name),
+        value: genRandomString(valueLen, cookieAllowedChars.value)
+      }
+
+      cookies.push(c)
+    }
+
+    const resp = await request(cookies)
+
+    const respCookies = resp.body.load.cookies
+
+    expect(respCookies).toBeDefined()
+
+    for (let i = 0; i < cookies.length; i++) {
+      const currCookie = cookies[i]
+      expect(respCookies[currCookie.name]).toBe(currCookie.value)
+    }
   })
 })
