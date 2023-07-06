b421a00eb9b6c0c2a9e2a0863c6a9b19b56b38cf
Borrax <borrax@protonmail.com>
2023-07-06 16:11:34 +0300

    JSdoc comment for the cookie config object

--- server/src/configs/cookieConfig.ts
+++ server/src/configs/cookieConfig.ts
@@ -1,3 +1,12 @@
+/**
+* Contains specifications for the cookies that are
+* sent to the server and which the cookie parser
+* middleware uses
+* @property allowedChars - contains regexes that test
+* positive for valid cookie names and values
+* @property maxHeaderSize - the maximum number of bytes
+* that the cookie header of the request can have
+*/
 export const cookieConfig = Object.freeze({
   allowedChars: {
     // eslint-disable-next-line no-control-regex
