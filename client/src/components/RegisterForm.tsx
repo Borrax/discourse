f497a2b8518ebd592b55cf5752eaf2ac9c60b03f
Borrax <borrax@protonmail.com>
2023-11-23 12:54:23 +0200

    Registration form password validation

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -13,13 +13,16 @@ enum RegFormFields {
 
 export const RegisterForm = (): JSX.Element => {
   const usernameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
 
   const {
     MAX_USERNAME_LEN,
-    MIN_USERNAME_LEN
+    MIN_USERNAME_LEN,
+    MIN_PASSWORD_LEN,
+    MAX_PASSWORD_LEN
   } = allowedUserRegLengths
 
-  const { USERNAME_REGEX } = regDataValidationRegex
+  const { USERNAME_REGEX, PASSWORD_REGEX } = regDataValidationRegex
 
   const getUsernameValidationErr = (username: string | undefined | null): string | null => {
     if (username == null || username.length === 0) {
@@ -38,6 +41,23 @@ export const RegisterForm = (): JSX.Element => {
     return null
   }
 
+  const getPasswordValidationErr = (password: string | undefined | null): string | null => {
+    if (password == null || password.length === 0) {
+      return 'Missing password'
+    }
+
+    if (password.length < MIN_PASSWORD_LEN ||
+      password.length > MAX_PASSWORD_LEN) {
+      return `Password must be between ${MIN_PASSWORD_LEN} and ${MAX_PASSWORD_LEN} long`
+    }
+
+    if (!PASSWORD_REGEX.test(password)) {
+      return 'The password is having forbidden symbols'
+    }
+
+    return null
+  }
+
   const showError = (errMsg: string, _field: RegFormFields): void => {
     console.log(errMsg)
   }
@@ -49,19 +69,28 @@ export const RegisterForm = (): JSX.Element => {
   const handleRegSubmit = (e: FormEvent): void => {
     e.preventDefault()
     let username = usernameRef.current?.value
+    let password = passwordRef.current?.value
 
     const usernameErr = getUsernameValidationErr(username)
 
-    if (usernameErr != null) {
+    if (usernameErr !== null) {
       showError(usernameErr, RegFormFields.username)
       return
     }
 
+    const passwordErr = getPasswordValidationErr(password)
+
+    if (passwordErr !== null) {
+      showError(passwordErr, RegFormFields.password)
+      return
+    }
+
     username = username as string
+    password = password as string
 
     tryRegister({
       username,
-      password: null
+      password
     })
   }
 
@@ -73,7 +102,7 @@ export const RegisterForm = (): JSX.Element => {
       </div>
       <div className="field-container">
         <input name={RegFormFields.password} type="password"
-          placeholder='Password'/>
+          placeholder='Password' ref={passwordRef}/>
       </div>
       <input type="submit" value="Register" />
   </form>
