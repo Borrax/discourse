977fb13b978ab5bfb32000acb944cedc47befcb6
Borrax <borrax@protonmail.com>
2023-11-29 21:18:48 +0200

    Jsdoc for the client side post method

--- client/src/apis/httpRequestMethods.ts
+++ client/src/apis/httpRequestMethods.ts
@@ -1,3 +1,10 @@
+/**
+* @function Makes an http POST request to the server
+*
+* @param url - The route the post request needs to be done to
+* @param payload - A string which will be in the body of the request
+* @returns A promise with the server's response
+*/
 export const post = async (url: string, payload: string):
 Promise<Response> => {
   return await fetch(url, {
