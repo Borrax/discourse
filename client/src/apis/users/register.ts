369c0586dffb654787ddff91503580d8baadf115
Borrax <borrax@protonmail.com>
2023-11-29 23:39:15 +0200

    Adding jsdoc comment to the user register client method

--- client/src/apis/users/register.ts
+++ client/src/apis/users/register.ts
@@ -3,6 +3,11 @@ import type { UserRegEntry, UserRegResponse } from '../../../../shared/types/Use
 import { apiPaths } from '../../../../shared/apiPaths'
 import { post } from '../httpRequestMethods'
 
+/**
+* @function Sends a request to the server to register the user
+*
+* @returns A promise with the server's response
+*/
 export const registerUser = async (data: UserRegEntry):
 Promise<UserRegResponse> => {
   return await post(apiPaths.user.register, JSON.stringify(data))
