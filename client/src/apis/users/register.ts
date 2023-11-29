9a9f515e518c3c3b5af53d7457f3d37c9134a48f
Borrax <borrax@protonmail.com>
2023-11-29 23:36:30 +0200

    Few corrections for the register user api method

--- client/src/apis/users/register.ts
+++ client/src/apis/users/register.ts
@@ -1,11 +1,10 @@
-import type { UserRegEntry } from '../../../../shared/types/UserSharedTypes'
+import type { UserRegEntry, UserRegResponse } from '../../../../shared/types/UserSharedTypes'
 
 import { apiPaths } from '../../../../shared/apiPaths'
 import { post } from '../httpRequestMethods'
-import { UserRegResponse } from '../../../../shared/types/UserSharedTypes'
 
 export const registerUser = async (data: UserRegEntry):
-  Promise<UserRegResponse> => {
-    return await post(apiPaths.user.register, JSON.stringify(data))
-              .then(res => res.json())
+Promise<UserRegResponse> => {
+  return await post(apiPaths.user.register, JSON.stringify(data))
+    .then(async res => await res.json())
 }
