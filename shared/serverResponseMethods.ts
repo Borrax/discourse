34627fc5da9e50753b933ba4e0bfdba702436d46
Borrax <borrax@protonmail.com>
2023-12-01 22:57:29 +0200

    Changing the accepted type in the error and success response determinators

--- shared/serverResponseMethods.ts
+++ shared/serverResponseMethods.ts
@@ -1,13 +1,13 @@
 import type { ErrorResponse, SuccessResponse } from './types/ServerResponseTypes'
 
-export const isSuccessResponseObj = <T>(obj: SuccessResponse<T> | null): obj is SuccessResponse<T> => {
+export const isSuccessResponseObj = <T>(obj: any): obj is SuccessResponse<T> => {
   if (obj === null) return false
   if ('load' in obj) return true
 
   return false
 }
 
-export const isErrorResponseObj = (obj: ErrorResponse | null): obj is ErrorResponse=> {
+export const isErrorResponseObj = (obj: any): obj is ErrorResponse=> {
   if (obj === null) return false
   if ('err' in obj) return true
 
