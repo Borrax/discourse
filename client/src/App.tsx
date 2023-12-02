881ce87a84faf70968799188db3a9d141295f2d6
Borrax <borrax@protonmail.com>
2023-12-02 22:26:14 +0200

    Adding buttons and changing the home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
-import { RegisterForm } from './components/RegisterForm'
 import { Home } from './pages/Home'
 
 export const App = (): JSX.Element => {
   return (
   <>
-    <h1>This is the app component</h1>
     <Home />
-    <RegisterForm />
   </>
   )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react'
+
 export const Home = (): JSX.Element => {
+  const [showForms, setShowForms] = useState({
+    register: false,
+    login: false
+  })
+
+  const toggleForms = (isRegForm: boolean): void => {
+    if (isRegForm) {
+      setShowForms({
+        register: !showForms.register,
+        login: false
+      })
+
+      return
+    }
+
+    setShowForms({
+      login: !showForms.login,
+      register: false
+    })
+  }
+
   return (
     <section>
-      <h2>This is the Home component</h2>
+      <h1>DISCOURSE</h1>
+      <div>
+        <button onClick={() => { toggleForms(false) }}>
+          Log In
+        </button>
+        <button onClick={() => { toggleForms(true) }}>
+          Register
+        </button>
+      </div>
     </section>
   )
 }
