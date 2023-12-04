00b66ad331143db9477d5415aa92f0178dc6424d
Borrax <borrax@protonmail.com>
2023-12-04 23:57:50 +0200

    Adding a button that shows password

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,16 +1,30 @@
-import type { Ref } from 'react'
+import type { RefObject } from 'react'
+
+import { useState } from 'react'
 
 type FormFieldProps = {
   name: string
   inputType: 'text' | 'password' | 'email'
-  inputRef: Ref<HTMLInputElement>
+  inputRef: RefObject<HTMLInputElement>
 }
 
 export const FormField = (props: FormFieldProps): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const toggleShowPassword = (): void => {
+    setShowPassword(!showPassword)
+  }
   return (
     <div className="field-container">
       <input name={props.name} ref={props.inputRef}
-        type={props.inputType} placeholder={props.name}/>
+        type={ showPassword
+          ? 'text'
+          : 'password'
+        } placeholder={props.name}/>
+      { props.inputType === 'password'
+        ? <button onClick={toggleShowPassword}>&#x1F441;</button>
+        : null
+      }
     </div>
   )
 }
